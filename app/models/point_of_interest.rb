f99b4487c9359aa80adcb47c41d12b4a8dc87cf2
Kye Bracey <kye.bracey@gmail.com>
2017-08-23 17:10:54 +0100

    Adds validations to point_of_interest model (all columns required)

--- app/models/point_of_interest.rb
+++ app/models/point_of_interest.rb
@@ -1,3 +1,5 @@
 class PointOfInterest < ApplicationRecord
   belongs_to :location
+
+  validates_presence_of :name, :buildingtype, :description
 end
--- spec/models/point_of_interest_spec.rb
+++ spec/models/point_of_interest_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe PointOfInterest, type: :model do
   it { is_expected.to be }
   it { is_expected.to belong_to(:location) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :buildingtype }
+  it { should validate_presence_of :description }
 end
