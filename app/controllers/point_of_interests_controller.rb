2f9491eba067c7a2fe329c2b4b42a5321aad81bc
Kye Bracey <kye.bracey@gmail.com>
2017-08-23 17:51:39 +0100

    Refactor: adds private method set_location in PointOfInterestsController

--- app/controllers/point_of_interests_controller.rb
+++ app/controllers/point_of_interests_controller.rb
@@ -1,18 +1,18 @@
 class PointOfInterestsController < ApplicationController
   def index
-    location = Location.find_by_id(params[:location_id])
+    location = set_location
     point_of_interests = location.point_of_interests.all
     render json: { data: point_of_interests }, status: :ok
   end
 
   def show
-    location = Location.find_by_id(params[:location_id])
+    location = set_location
     point_of_interest = location.point_of_interests.find_by_id(params[:id])
     render json: { data: point_of_interest }, status: :ok
   end
 
   def create
-    location = Location.find_by_id(params[:location_id])
+    location = set_location
     point_of_interest = location.point_of_interests.create(point_of_interest_params)
     render json: { data: point_of_interest }, status: :ok
   end
@@ -20,5 +20,9 @@ class PointOfInterestsController < ApplicationController
   private
   def point_of_interest_params
     params.permit(:name, :buildingtype, :description)
+  end
+
+  def set_location
+    Location.find_by_id(params[:location_id])
   end
 end
